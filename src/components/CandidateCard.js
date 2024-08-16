98715a7a3be6b398e2f29d2dadb2be603532e2b5
Agus <agustin.modugno@arex.io>
2024-08-16 17:59:19 -0300

    add more examples and responsiveness

--- src/components/CandidateCard.js
+++ src/components/CandidateCard.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
 const CandidateCard = ({ name, description, votes, onVote, hasVoted }) => {
   return (
-    <div className="candidate-card">
+    <Grid xs={12} sm={6} md={4} lg={2} 
+      sx={{ border: '2px solid grey' }} 
+      justifyContent="center"
+      alignItems="center"
+    >
       <h2>{name}</h2>
       <p>{description}</p>
       <p>Votos: {votes}</p>
       <Button variant="contained" disabled={hasVoted} onClick={onVote}>Votar</Button>
-    </div>
+    </Grid>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import Header from '../components/Header';
 import CandidateCard from '../components/CandidateCard';
 import VotedCandidateCard from '../components/VotedCandidateCard';
@@ -7,7 +9,13 @@ const Home = () => {
   const [candidates, setCandidates] = useState([
     { id: 1, name: 'Candidato 1', description: 'Descripción 1', votes: 0 },
     { id: 2, name: 'Candidato 2', description: 'Descripción 2', votes: 0 },
-    { id: 3, name: 'Candidato 3', description: 'Descripción 3', votes: 0 }
+    { id: 3, name: 'Candidato 3', description: 'Descripción 3', votes: 0 },
+    { id: 4, name: 'Candidato 4', description: 'Descripción 4', votes: 0 },
+    { id: 5, name: 'Candidato 5', description: 'Descripción 5', votes: 0 },
+    { id: 6, name: 'Candidato 6', description: 'Descripción 6', votes: 0 },
+    { id: 7, name: 'Candidato 7', description: 'Descripción 7', votes: 0 },
+    { id: 8, name: 'Candidato 8', description: 'Descripción 8', votes: 0 },
+    { id: 9, name: 'Candidato 9', description: 'Descripción 9', votes: 0 }
   ]);
 
   const [vote, setVote] = useState(0)
@@ -40,7 +48,7 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <div className="candidates-list">
+      <Grid container spacing={4} className="candidates-list">
         {candidates.map(candidate => (
           <CandidateCard
             key={candidate.id}
@@ -54,10 +62,10 @@ const Home = () => {
             }}
           />
         ))}
-      </div>
-      <div className="candidate-vote">
+      </Grid>
+      <Container className="candidate-vote">
           <VotedCandidateCard id={vote} />
-      </div>
+      </Container>
     </div>
   );
 };
