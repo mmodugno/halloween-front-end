944ee9cc015529b496e93ef1d89de98259a59525
Magali Modugno <magalimodugno@gmail.com>
2024-08-20 23:40:35 -0300

    card formatting

--- src/components/CandidateCard.js
+++ src/components/CandidateCard.js
@@ -1,22 +1,50 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
+import React from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
-const CandidateCard = ({ name, description, votes, onVote, hasVoted, disableVoteButton }) => {
+const CandidateCard = ({
+  name,
+  description,
+  votes,
+  onVote,
+  hasVoted,
+  disableVoteButton,
+}) => {
   return (
-    <Grid item xs={12} sm={6} md={4} lg={2} >
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <Button variant="contained"
-      disabled={hasVoted}
-      onClick={onVote}
-      style={{
-        backgroundColor: disableVoteButton ? 'grey' : undefined,
-        color: 'white',
-        cursor: disableVoteButton ? 'not-allowed' : 'pointer',
-      }}>
-        {disableVoteButton ? "No seas tramposo" : 'Votar'}
-        </Button>
+    <Grid item xs={12} sm={6} md={4} lg={2}>
+      <Card
+        variant="outlined"
+        style={{
+          backgroundColor: "#f5f5f5",
+          color: "#000",
+          borderRadius: "18px",
+          border: "1px solid #ddd",
+        }}
+      >
+        <CardContent>
+          <Typography variant="h6" component="div" gutterBottom style={{ fontWeight: 'bold' }}>
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {description}
+          </Typography>
+          <Button
+            variant="contained"
+            disabled={hasVoted || disableVoteButton}
+            onClick={onVote}
+            style={{
+              marginTop: '10px',
+              color: "white",
+              cursor: disableVoteButton ? "not-allowed" : "pointer",
+            }}
+          >
+            {disableVoteButton ? "No seas tramposo" : "Votar"}
+          </Button>
+        </CardContent>
+      </Card>
     </Grid>
   );
 };
